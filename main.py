eb1e3fd015e36bc14c01714e54bd308328c1b908
Sam Branka <sdbranka@me.com>
2023-05-12 15:14:27 -0400

    adding notes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from pandas.plotting import autocorrelation_plot          # for auto regressive model
-from pandas.tseries.offsets import DateOffset
+from pandas.tseries.offsets import DateOffset             # create date offset to make predictions into the future
 from statsmodels.graphics.api import qqplot
 from statsmodels.graphics.tsaplots import plot_acf,plot_pacf
 from statsmodels.tsa.arima.model import ARIMA             # For non-seasonal data
@@ -18,49 +18,38 @@ from statsmodels.tsa.stattools import adfuller            # Testing For Stationa
 # Autoregressive Integrated Moving Averages
 # The general process for ARIMA models is the following:
 
-# Visualize the Time Series Data
-
-# Make the time series data stationary
-# Plot the Correlation and AutoCorrelation Charts
-# Construct the ARIMA Model or Seasonal ARIMA based on the data
-# - lags is the amount of time prediction is based upon
-# Use the model to make predictions
+# Step 1: Visualize the Time Series Data
+# Step 2: Make the time series data stationary
+# Step 3: Plot the Correlation and AutoCorrelation Charts
+# Step 4: Construct the ARIMA Model or Seasonal ARIMA based on the data
+#         - lags is the amount of time the prediction is based upon
+# Step 5: Use the model to make predictions
 
 # read the data
-df = pd.read_csv("perrin-freres-monthly-champaigne.csv")
+df = pd.read_csv("Data/perrin-freres-monthly-champaigne.csv")
 
 
 # print(df.shape)
 # (105, 2)
 
 # print(df.head())
-#     Month  Perrin Freres monthly champagne sales millions ?64-?72
-# 0  1964-01
-#       2815
-# 1  1964-02
-#       2672
-# 2  1964-03
-#       2755
-# 3  1964-04
-#       2721
-# 4  1964-05
-#       2946
+#      Month  Perrin Freres monthly champagne sales millions ?64-?72
+# 0  1964-01                                               2815
+# 1  1964-02                                               2672
+# 2  1964-03                                               2755
+# 3  1964-04                                               2721
+# 4  1964-05                                               2946
 
 # print(df.tail())
-#       Month  Perrin Freres monthly champagne sales millions ?64-?72
-# 100  1972-05
-#         4618
-# 101  1972-06
-#         5312
-# 102  1972-07
-#         4298
-# 103  1972-08
-#         1413
-# 104  1972-09
-#         5877
+#        Month  Perrin Freres monthly champagne sales millions ?64-?72
+# 100  1972-05                                               4618
+# 101  1972-06                                               5312
+# 102  1972-07                                               4298
+# 103  1972-08                                               1413
+# 104  1972-09                                               5877
 
 
-## Cleaning up the data
+### --------- Cleaning up the data --------- ###
 # change names of columns
 df.columns=["Month","Sales"]
 # print(df.head())
@@ -71,13 +60,16 @@ df.columns=["Month","Sales"]
 # 3  1964-04   2721
 # 4  1964-05   2946
 
-# ## Drop last 2 rows
+# Remove NaN values
+# Drop last 2 rows
 # I did this through the csv so unneccessary in code
 # df.drop(106,axis=0,inplace=True)
 # df.drop(105,axis=0,inplace=True)
 # print(df.tail())
 
-# Convert Month into Datetime
+# Convert Month into Datetime objects
+# when pandas reads in the date it does so as a string, we will need to convert
+# this to a workable format for our needs
 df['Month']=pd.to_datetime(df['Month'])
 # print(df.head())
 #        Month  Sales
@@ -111,28 +103,37 @@ df.set_index('Month',inplace=True)
 # max    13916.000000
 
 
-# Step 2
-# visualize the data
+### --------- Visualize the Data --------- ###
 # chart1
 # df.plot()
 # plt.title("Chart 1")
 # plt.show()
 
 
-### Testing For Stationarity
+### --------- Testing For Stationarity --------- ###
 # our goal will be to make the data stationary. The graph shows a high degree
 # of influence from the seasons with the summers appearing as positive peaks 
 # and the winters appearing as negative peaks, which makes sense given the 
 # growing season
+
+# perform Dickey-Fuller test
+# most important vlue is the p-value
+# The Dickey-Fuller test is a type of hypothesis testing where:
+# - Ho: It is non stationary
+# - H1: It is stationary
+# Basically if the p-value is less than 0.05 than the data is stationary, if
+# it is greater than 0.05 then it is non-stationary and will require further
+# work
 test_result = adfuller(df['Sales'])
 # print(test_result)
 # (-1.8335930563276184, 0.36391577166024713, 11, 93, 
 # {'1%': -3.502704609582561, '5%': -2.8931578098779522, 
 # '10%': -2.583636712914788}, 1478.4633060594724) 
 
-#Ho: It is non stationary
-#H1: It is stationary
 
+
+# function that outputs the results of the Dickey-Fuller in an easily readable 
+# format
 def adfuller_test(sales):
     result=adfuller(sales)
     labels = ['ADF Test Statistic','p-value','#Lags Used','Number of Observations Used']
@@ -153,6 +154,8 @@ def adfuller_test(sales):
 # has a unit root, indicating it is non-stationary   
 # None
 
+# If the data is non-stationary what do we do to make it stationary? First
+# thing that we can do is differencing
 
 # Differencing
 # print(df['Sales'].shift(1))
@@ -203,7 +206,9 @@ df['Seasonal First Difference'] = df['Sales']-df['Sales'].shift(12)
 # 1965-02-01   2475                   -66.0                     -197.0
 
 
-## Again test dickey fuller test
+# Again test dickey fuller test
+# Make sure to use .dropna() to drop the NaN values otherwise you will have 
+# a lot of errors 
 # print(adfuller_test(df['Seasonal First Difference'].dropna()))
 # ADF Test Statistic : -7.626619157213166
 # p-value : 2.0605796968136632e-11
@@ -241,9 +246,14 @@ df['Seasonal First Difference'] = df['Sales']-df['Sales'].shift(12)
 # toward 0 in some manner. A clearer pattern for an MA model is in the ACF. 
 # The ACF will have non-zero autocorrelations only at lags involved in the 
 # model.
-# p,d,q p AR model lags d differencing q MA lags
+# p,d,q 
+# - p AR model lags 
+# - d differencing 
+# - q MA lags
 
 # chart4
+# .iloc set to 13 because we are using the Seasonal First Difference wherein the
+# first twelve rows contain NaN values
 # fig = plt.figure(figsize=(12,8))
 # ax1 = fig.add_subplot(211)
 # fig = sm.graphics.tsa.plot_acf(df['Seasonal First Difference'].iloc[13:],lags=40,ax=ax1)
@@ -254,7 +264,10 @@ df['Seasonal First Difference'] = df['Sales']-df['Sales'].shift(12)
 
 
 # For non-seasonal data
-#p=1, d=1, q=0 or 1
+# looking at the above graph we see the shut off (the drop from the first and highest
+# value to the first point that starts the continuous series) occurs around 1, 
+# so p=1. We are looking at only a single seasonal shift so with our differencing
+# d=1. q=0 or 1. so order will = (p,d,q)
 model=ARIMA(df['Sales'],order=(1,1,1))
 model_fit=model.fit()
 # print(model_fit.summary())
@@ -291,6 +304,9 @@ model_fit=model.fit()
 # plt.show()
 
 
+### --------- For Seasonal Data, SARIMAX --------- ###
+
+# the seasonal_order = (p,d,q,[the number of values shifted])
 model = sm.tsa.statespace.SARIMAX(df['Sales'],order=(1, 1, 1),seasonal_order=(1,1,1,12))
 results = model.fit()
 # RUNNING THE L-BFGS-B CODE
@@ -336,7 +352,8 @@ results = model.fit()
 # CONVERGENCE: REL_REDUCTION_OF_F_<=_FACTR*EPSMCH
 
 
-
+# check to see accuracy of the model by using known values, predict start 
+# from row 90 of the data through row 103 of the data
 # chart6
 # df['forecast']=results.predict(start=90,end=103,dynamic=True)
 # df[['Sales','forecast']].plot(figsize=(12,8))
@@ -344,9 +361,30 @@ results = model.fit()
 # plt.show()
 
 
+# Predict for dates into the future 
+# print(df.index[-1])
+# 1972-09-01 00:00:00
+
+# create a variable future_dates that looks from the last date from our data to 
+# 24 months into the future
 future_dates = [df.index[-1]+ DateOffset(months=x)for x in range(0,24)]
+# print(f"future_dates: \n{future_dates}")
+# future_dates:
+# [Timestamp('1972-09-01 00:00:00'), Timestamp('1972-10-01 00:00:00'), Timestamp('
+# 1972-11-01 00:00:00'), Timestamp('1972-12-01 00:00:00'), Timestamp('1973-01-01 0
+# 0:00:00'), Timestamp('1973-02-01 00:00:00'), Timestamp('1973-03-01 00:00:00'), T
+# imestamp('1973-04-01 00:00:00'), Timestamp('1973-05-01 00:00:00'), Timestamp('19
+# 73-06-01 00:00:00'), Timestamp('1973-07-01 00:00:00'), Timestamp('1973-08-01 00:
+# 00:00'), Timestamp('1973-09-01 00:00:00'), Timestamp('1973-10-01 00:00:00'), Tim
+# estamp('1973-11-01 00:00:00'), Timestamp('1973-12-01 00:00:00'), Timestamp('1974
+# -01-01 00:00:00'), Timestamp('1974-02-01 00:00:00'), Timestamp('1974-03-01 00:00
+# :00'), Timestamp('1974-04-01 00:00:00'), Timestamp('1974-05-01 00:00:00'), Times
+# tamp('1974-06-01 00:00:00'), Timestamp('1974-07-01 00:00:00'), Timestamp('1974-0
+# 8-01 00:00:00')]
+
+# create a dataframe of the newly built future_dates
 future_datest_df = pd.DataFrame(index=future_dates[1:],columns=df.columns)
-print(future_datest_df.tail())
+# print(future_datest_df.tail())
 #            Sales Sales First Difference Seasonal First Difference
 # 1974-04-01   NaN                    NaN                       NaN
 # 1974-05-01   NaN                    NaN                       NaN
@@ -354,12 +392,18 @@ print(future_datest_df.tail())
 # 1974-07-01   NaN                    NaN                       NaN
 # 1974-08-01   NaN                    NaN                       NaN
 
+# add the new dataframe to the original dataframe as rows in a new dataframe
+future_df = pd.concat([df,future_datest_df])
+# use the model to create predictions for the future dates
+future_df['forecast'] = results.predict(start = 104, end = 129, dynamic= True)  
+
+# future_df.to_csv("Data/future_df.csv")
+
+
 
 # chart7
-future_df = pd.concat([df,future_datest_df])
-future_df['forecast'] = results.predict(start = 104, end = 120, dynamic= True)  
-future_df[['Sales', 'forecast']].plot(figsize=(12, 8)) 
-plt.title("Chart 7 Sales vs Forecast")
-plt.show()
+# future_df[['Sales', 'forecast']].plot(figsize=(12, 8)) 
+# plt.title("Chart 7 Sales vs Forecast")
+# plt.show()
 
 
